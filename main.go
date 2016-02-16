e5c6eaf5cb6ca55ace1d979a80250dfa9e4b501d
Mihai Scurtu <mihai.scurtu@chinookbook.net>
2016-02-16 14:09:30 +0200

    Wrap around server

--- main.go
+++ main.go
@@ -5,28 +5,41 @@ import (
 	"math/rand"
 	"time"
 	"github.com/mihai-scurtu/mishu/mishu"
+	"net/http"
+	"os"
+	"log"
 )
 
 
 
 func main() {
-	rand.Seed(time.Now().Unix())
-	words := mishu.GetWordList("./data/raw/*")
+	port := os.Getenv("PORT")
 
-	target := 64
-	poem := mishu.GeneratePoem(words, target)
-
-	measure := 8
-	syl := 0
-	for _, word := range poem {
-		if syl > measure {
-			syl = 0;
-			fmt.Print("\n")
-		}
-
-		fmt.Printf("%s ", word)
-		syl += len(mishu.Syllables(word));
+	if port == "" {
+		port = ":8000"
 	}
 
-	fmt.Print("\n")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rand.Seed(time.Now().Unix())
+		words := mishu.GetWordList("./data/raw/*")
+
+		target := 64
+		poem := mishu.GeneratePoem(words, target)
+
+		measure := 8
+		syl := 0
+		for _, word := range poem {
+			if syl > measure {
+				syl = 0;
+				fmt.Fprint(w, "\n")
+			}
+
+			fmt.Fprintf(w, "%s ", word)
+			syl += len(mishu.Syllables(word));
+		}
+
+		fmt.Fprint(w, "\n")
+	});
+
+	log.Fatal(http.ListenAndServe(port, nil))
 }
